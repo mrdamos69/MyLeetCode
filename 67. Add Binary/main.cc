973a1587af8d1134d876003496d928c700a0193e
Dmitrii Chichkanov <86652230+mrdamos69@users.noreply.github.com>
2023-12-02 01:30:40 -0800

    add task 67

---
+++ 67. Add Binary/main.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+
+class Solution {
+public:
+    std::string addBinary(std::string a, std::string b) {
+        std::string result = "";
+        bool mem = false; 
+        int allSize = a.size() > b.size() ? a.size() - (b.size()) : b.size() - (a.size());
+        if(a == b && a != "0") return a + "0";
+        else if(a == b && a == "0") return a;
+        if (a.size() > b.size()) {
+            for (int i = 0; i < allSize; i++) 
+                b = "0" + b;
+        } else if(a.size() < b.size()) {
+            for (int i = 0; i < allSize; i++)
+                a = "0" + a;
+        }
+        std::cout << a << "   " << b << std::endl;
+
+        for (int i = (a.size() - 1) ; i >= 0; i--) {
+            if(mem && a[i] < b[i]) {
+                a[i] = '1';
+                mem = false;
+            } else if(mem && a[i] > b[i]) {
+                b[i] = '1';
+                mem = false;
+            }
+            if(a[i] < b[i] || a[i] > b[i]) {
+                result = "1" + result;
+            } else if(a[i] == '1' && a[i] == b[i]) {
+                result = "0" + result;
+                mem = true;
+            } else if(mem && (a[i] == '0' && a[i] == b[i])){
+                result = "1" + result;
+            } else {
+                result = "0" + result;
+            }
+        }
+        if(mem) result = "1" + result;
+
+        return result;
+    }
+};
---
+++ 67. Add Binary/unit_test.cc
@@ -0,0 +1,42 @@
+#include <gtest/gtest.h>
+#include "main.cc"
+
+TEST(Test_67, test_1) {
+    Solution test;
+    ASSERT_EQ(test.addBinary("11", "1"), "100");
+}
+
+TEST(Test_67, test_2) {
+    Solution test;
+    ASSERT_EQ(test.addBinary("1010", "1011"), "10101");
+}
+
+TEST(Test_67, test_3) {
+    Solution test;
+    ASSERT_EQ(test.addBinary("1", "111"), "1000");
+}
+
+TEST(Test_67, test_4) {
+    Solution test;
+    ASSERT_EQ(test.addBinary("1111", "1111"), "11110");
+}
+
+TEST(Test_67, test_5) {
+    Solution test;
+    ASSERT_EQ(test.addBinary("0", "0"), "0");
+}
+
+TEST(Test_67, test_6) {
+    Solution test;
+    ASSERT_EQ(test.addBinary("100", "110010"), "110110");
+}
+
+TEST(Test_67, test_7) {
+    Solution test;
+    ASSERT_EQ(test.addBinary("101111", "10"), "110001");
+}
+
+int main(int argc, char *argv[]) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
