6ac943c9662745b6806bb6f275fd7ae2d38540fb
Dmitrii Chichkanov <86652230+mrdamos69@users.noreply.github.com>
2023-12-07 23:23:28 -0800

    add  task 141

--- 141. Linked List Cycle/main.cc
+++ 141. Linked List Cycle/main.cc
@@ -7,20 +7,37 @@
      ListNode(int x) : val(x), next(NULL) {}
 };
 
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+//         if(head != NULL) {
+//             ListNode* temp = head;
+//             size_t count = 0;
+//             while (temp->next != NULL) {
+//                 ListNode* temp2 = head;
+//                 for (size_t i = 0; i <= count; i++) {
+//                     if(temp2 == temp->next) return true;
+//                     temp2 = temp2->next;
+//                 }
+//                 temp = temp->next;
+//                 count++;
+//             }
+//         } 
+//         return false;
+//     }
+// };
+
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(head != NULL) {
-            ListNode* temp = head;
-            while (temp->next != NULL) {
-                ListNode* temp2 = head;
-                while (temp2->next != NULL) {
-                    if(temp2 == temp) return true;
-                    temp2 = temp2->next;
-                }
-                temp = temp->next;
-            }
-        } 
+        ListNode *temp = head;
+        ListNode *temp2 = head;
+        while (temp != NULL && temp->next != NULL) {
+            temp = temp->next->next;
+            temp2 = temp2->next;
+            if(temp == temp2) return true;
+
+        }
         return false;
     }
 };
--- 141. Linked List Cycle/unit_test.cc
+++ 141. Linked List Cycle/unit_test.cc
@@ -3,8 +3,47 @@
 
 TEST(Test_141, test_1) {
     Solution test;
-    ListNode *testList = nullptr;
-    ASSERT_EQ(test.hasCycle(testList), false);
+    ListNode* node = new ListNode(1);
+    node->next = new ListNode(2);
+    node->next->next = node;
+    ASSERT_EQ(test.hasCycle(node), true);
+    delete node->next;
+    delete node;
+}
+
+TEST(Test_141, test_2) {
+    Solution test;
+    ListNode* node = new ListNode(1);
+    node->next = new ListNode(2);
+    ASSERT_EQ(test.hasCycle(node), false);
+    delete node->next;
+    delete node;
+}
+
+TEST(Test_141, test_3) {
+    Solution test;
+    ListNode* node = new ListNode(-21);
+    std::vector<int> vec {-21,10,17,8,4,26,5,35,33,-7,-16,27,-12,6,29,-12,5,9,20,14,14,2,13,-24,21,23,-21,5};
+    ListNode* temp = node;
+
+    for(size_t i = 1; i < vec.size(); i++) {
+      temp->next = new ListNode(vec[i]);
+      temp = temp->next;
+    }
+    ASSERT_EQ(test.hasCycle(node), false);
+}
+
+TEST(Test_141, test_4) {
+    Solution test;
+    ListNode* node = new ListNode(-21);
+    std::vector<int> vec {1, 2, 3};
+    ListNode* temp = node;
+
+    for(size_t i = 1; i < vec.size(); i++) {
+      temp->next = new ListNode(vec[i]);
+      temp = temp->next;
+    }
+    ASSERT_EQ(test.hasCycle(node), false);
 }
 
 int main(int argc, char *argv[]) {
