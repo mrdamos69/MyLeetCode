bc88375f769a2c64797e95565fba1e2c9c2e8c3f
Dmitrii Chichkanov <86652230+mrdamos69@users.noreply.github.com>
2024-01-16 01:31:32 -0800

    adding iterative approach in task number 94

--- 94. Binary Tree Inorder Traversal/main.cc
+++ 94. Binary Tree Inorder Traversal/main.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 
  struct TreeNode {
     int val;
@@ -10,18 +11,39 @@
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  };
  
+// class Solution {
+// public:
+//     std::vector<int> inorderTraversal(TreeNode* root) {
+//         std::vector<int> result;
+//         lookingTree(root, result);
+//         return result;
+//     }
+
+//     void lookingTree(TreeNode* root, std::vector<int> &result) {
+//         if(root == nullptr) return;
+//         lookingTree(root->left, result);
+//         result.push_back(root->val);
+//         lookingTree(root->right, result);
+//     }
+// };
+
 class Solution {
 public:
     std::vector<int> inorderTraversal(TreeNode* root) {
         std::vector<int> result;
-        lookingTree(root, result);
-        return result;
-    }
+        std::stack<TreeNode*> temp_stack;
+        TreeNode* count = root;
 
-    void lookingTree(TreeNode* root, std::vector<int> &result) {
-        if(root == nullptr) return;
-        lookingTree(root->left, result);
-        result.push_back(root->val);
-        lookingTree(root->right, result);
+        while(count != nullptr || !temp_stack.empty()) {
+            while (count != nullptr) {
+                temp_stack.push(count);
+                count = count->left;
+            }
+            count = temp_stack.top();
+            temp_stack.pop();
+            result.push_back(count->val);
+            count = count->right;
+        }
+        return result;
     }
 };
