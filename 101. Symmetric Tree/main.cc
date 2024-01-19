addb5c246e6d52f058d36f8d38a4261620792ad4
Dmitrii Chichkanov <86652230+mrdamos69@users.noreply.github.com>
2024-01-19 01:05:31 -0800

    adding task number 101

---
+++ 101. Symmetric Tree/main.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+ struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+class Solution {
+public:
+    bool isSymmetricHelp(TreeNode* left, TreeNode* right) {
+        if (left == nullptr && right == nullptr) return true;
+        if (left == nullptr || right == nullptr) return false;
+        return (left->val == right->val) && isSymmetricHelp(left->left, right->right) && isSymmetricHelp(left->right, right->left);
+    }
+    bool isSymmetric(TreeNode* root) {
+        return root == nullptr || isSymmetricHelp(root->left, root->right);
+    }
+};
---
+++ 101. Symmetric Tree/unit_test.cc
@@ -0,0 +1,53 @@
+#include <gtest/gtest.h>
+#include "main.cc"
+
+TEST(Test_101, test_1) {
+  Solution test;
+  TreeNode *root_1 = new TreeNode(1);
+  root_1->left = new TreeNode(2);
+  root_1->right = new TreeNode(2);
+  root_1->left->left = new TreeNode(3);
+  root_1->right->right = new TreeNode(3);
+  root_1->left->right = new TreeNode(4);
+  root_1->right->left = new TreeNode(4);
+  ASSERT_EQ(test.isSymmetric(root_1), true);
+}
+
+TEST(Test_101, test_2) {
+  Solution test;
+  TreeNode *root_1 = new TreeNode(1);
+  root_1->left = new TreeNode(2);
+  root_1->right = new TreeNode(2);
+  root_1->left->right = new TreeNode(3);
+  root_1->right->right = new TreeNode(3);
+  ASSERT_EQ(test.isSymmetric(root_1), false);
+}
+
+TEST(Test_101, test_3) {
+  Solution test;
+  TreeNode *root_1 = new TreeNode(1);
+  root_1->left = new TreeNode(2);
+  root_1->right = new TreeNode(2);
+  root_1->left->left = new TreeNode(3);
+  root_1->right->right = new TreeNode(3);
+  ASSERT_EQ(test.isSymmetric(root_1), true);
+}
+
+TEST(Test_101, test_4) {
+  Solution test;
+  TreeNode *root_1 = new TreeNode(1);
+  ASSERT_EQ(test.isSymmetric(root_1), true);
+}
+
+TEST(Test_101, test_5) {
+  Solution test;
+  TreeNode *root_1 = new TreeNode(1);
+  root_1->left = new TreeNode(2);
+  root_1->right = new TreeNode(3);
+  ASSERT_EQ(test.isSymmetric(root_1), false);
+}
+
+int main(int argc, char *argv[]) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
